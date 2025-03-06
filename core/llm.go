874038f4885381a0c7a7d60755de9a29b2e5c0f8
Guillaume de Rouville <guillaume@dagger.io>
2025-03-06 00:29:47 +0000

    mcp: hello world
    Co-authored-by: Guillaume de Rouville <guillaume@dagger.io>
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- core/llm.go
+++ core/llm.go
@@ -6,9 +6,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"sort"
 	"strings"
+	"time"
 
 	"dagger.io/dagger/telemetry"
 	"github.com/anthropics/anthropic-sdk-go"
@@ -17,6 +19,10 @@ import (
 	"github.com/dagger/dagger/engine/client/secretprovider"
 	"github.com/iancoleman/strcase"
 	"github.com/joho/godotenv"
+	"github.com/mark3labs/mcp-go/mcp"
+	mcpserver "github.com/mark3labs/mcp-go/server"
+	bkgwpb "github.com/moby/buildkit/frontend/gateway/pb"
+	"github.com/moby/buildkit/util/bklog"
 	"github.com/vektah/gqlparser/v2/ast"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/log"
@@ -540,6 +546,59 @@ func (llm *LLM) LastReply(ctx context.Context, dag *dagql.Server) (string, error
 		reply = txt
 	}
 	return reply, nil
+}
+
+func (llm *LLM) MCP(ctx context.Context, dag *dagql.Server) error {
+	bklog.G(ctx).Debugf("🎃 Starting MCP function")
+	s := mcpserver.NewMCPServer("Dagger", "0.0.1")
+	s.AddTool(
+		mcp.NewTool("toto",
+			mcp.WithDescription("A tool to greet someone."),
+			mcp.WithString("name", mcp.Required(), mcp.Description("Name of the person to greet")),
+		),
+		func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+			name, ok := request.Params.Arguments["name"].(string)
+			if !ok {
+				return nil, errors.New("name must be a string")
+			}
+			return mcp.NewToolResultText(fmt.Sprintf("Hello %s!", name)), nil
+		})
+
+	bk, err := llm.Query.Buildkit(ctx)
+	if err != nil {
+		return fmt.Errorf("buildkit client error: %w", err)
+	}
+
+	// FIXME: do not use Terminal attachable
+	term, err := bk.OpenTerminal(ctx)
+	if err != nil {
+		return fmt.Errorf("open terminal error: %w", err)
+	}
+	defer term.Close(bkgwpb.UnknownExitStatus)
+	bklog.G(ctx).Debugf("🎃 Terminal opened")
+	go func() {
+		for {
+			select {
+			case <-term.ResizeCh:
+			case err := <-term.ErrCh:
+				bklog.G(ctx).Debugf("🎃 error: |%+v|", err)
+				return
+			}
+		}
+	}()
+
+	// Read from Stdin in a goroutine
+	go io.Copy(term.Stdout, term.Stdin)
+
+	select {
+	case <-ctx.Done():
+	case <-time.After(30 * time.Second):
+		bklog.G(ctx).Warnf("🎃 Timeout waiting for Stdin input")
+		return fmt.Errorf("timeout waiting for stdin input")
+	}
+
+	term.Close(0) // clean exit
+	return ctx.Err()
 }
 
 // send the context to the LLM endpoint, process replies and tool calls; continue in a loop
