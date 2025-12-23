a7fed46764ccbd05ed6f8fdb6672df0a2f9c66a7
Tibor Vass <teabee89@gmail.com>
2025-12-23 10:40:11 +0100

    core: add Check.Source to return the module source where the check is defined
    For toolchains, this returns the source of the toolchain where the check is defined.
    Otherwise, it coincides with the source of the module where the check is being run in.
    
    Check has a private Module field, but this commit adds an exported Source field.
    
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- sdk/go/dagger.gen.go
+++ sdk/go/dagger.gen.go
@@ -1213,6 +1213,15 @@ func (r *Check) Run() *Check {
 	}
 }
 
+// The module source where the check is defined (i.e., toolchains)
+func (r *Check) Source() *ModuleSource {
+	q := r.query.Select("source")
+
+	return &ModuleSource{
+		query: q,
+	}
+}
+
 type CheckGroup struct {
 	query *querybuilder.Selection
 
